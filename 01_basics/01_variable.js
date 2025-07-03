3864b1bd792dbfc68647dbe2173cccb6b26c62f8
shivani87838 <shivanis7513@gmail.com>
2025-07-03 07:07:55 +0000

    container config

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId =144553
+let accountEmail ="shivani@gmail.com"
+var accountPassword ="12345"
+accountCity ="jaipur"
+ let accountState;
+ //accountId=2;  //not allowed
+ accountEmail="shi@gmail.com"
+ accountPassword="21212121"
+ accountCity="Bangluru"
+ console.log(accountId);
+ /*prefer not to use var 
+ because of issue in block scope and function scope
+ */ 
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
