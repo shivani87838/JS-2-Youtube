6138d854ec8e209a5e329d35b3351a1af0ecf989
shivani87838 <shivanis7513@gmail.com>
2025-07-03 07:46:48 +0000

    datatypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as a newer version
+//alert(3+3)  //we are using node.js ,not browser
+console.log(3
+    +3) //code readability should be higher 
+console.log("Shivani")
+
+let name ="shivani"
+let age =18
+let isLoggedIn = false;
+
+// number=> 2 to the power 53
+//bigint
+// boolean=>true/False
+//string=>""
+//null=>standalone value
+// undefined
+//symbol => unique
+
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
