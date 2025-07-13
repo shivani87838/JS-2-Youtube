a7bae98232bb47b686bde4186e2b5e98b4333618
shivani87838 <shivanis7513@gmail.com>
2025-07-13 17:27:04 +0000

    Array in detail

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -9,4 +9,17 @@ const dc_heroes =["SuperMan","Flash","Batman"];
 //console.log(allHeroes);
  
 const all_new_Heroes =[...marvel_heroes,...dc_heroes]
-console.log(all_new_Heroes);
+//console.log(all_new_Heroes);
+
+const anotherArr = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_anoterArr = anotherArr.flat(Infinity);
+//console.log(real_anoterArr);
+console.log(Array.isArray("Hitesh"));
+console.log(Array.from("Hitesh"));
+console.log(Array.from({name:"Hitesh"}));   //intresting
+let score1 =100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
+
+
