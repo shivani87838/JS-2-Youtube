2ee737c916b36b9c7e367c749327d581f76314d5
shivani87838 <shivanis7513@gmail.com>
2025-07-23 16:37:02 +0000

    function in js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -29,4 +29,32 @@ function loginUserMessage(username = "sam") {
     return `${username} just logged in`
 }
 //console.log(loginUserMessage("Shivani"));
-console.log(loginUserMessage("Shivani"));
+//console.log(loginUserMessage("Shivani"));
+
+function calculateCartPrice(val1,val2, ...num1){
+    return num1
+}
+//console.log(calculateCartPrice(200,400,500,789,908));
+
+const user ={
+    username:"Hitesh",
+    price:199
+}
+
+function handleObject(anyobject){
+   // console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+ //handleObject(user)
+ handleObject({
+    username:"Sam",
+    price:399
+ })
+
+ const newArr=[200,400,500,600]
+ function returnSecondValue(getArray){
+       return getArray[1]
+ }
+
+//console.log(returnSecondValue(newArr));
+console.log(returnSecondValue([200,400,5000,50]));
