63956b1e22cc50820b4924a5befc4c73e8ff9032
shivani87838 <shivanis7513@gmail.com>
2025-07-26 08:55:06 +0000

    scopes in js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,17 +1,58 @@
 //var c =300;
-let a=300;
-
+//let a=300;
+/*
 if(true){
 let a =10
 const b= 20
- console.log("Inner a =",a);
+// console.log("Inner a =",a);
  
 }
 for (let i = 0; i < array.length; i++) {
     const element = array[i];
     
 }
-
-console.log(a);
+*/
+//console.log(a);
 //console.log(b);
 //console.log(c);
+
+
+function one() {
+    const userName="Hitesh"
+     function two()
+     {
+       const website="Youtube"
+        console.log(userName);
+     }
+     //console.log(website);
+
+     two()
+}
+//one();
+
+if (true) {
+    const userName="Hitesh"
+    if (userName==="Hitesh") {
+        const  website =" youtube"
+        //console.log(userName+website);
+    }
+    //console.log(website);
+    
+}
+//console.log(userName);
+
+
+//++++++++++++++++++++++  Interesting +++++++++++++++++++++++++++++++++++++++
+
+console.log(addOne(5));
+
+function addOne(num) {
+    return num+1;
+}
+
+
+addTwo(5)
+const addTwo = function(num) {
+    return num+2;
+}
+//addTwo(5);
