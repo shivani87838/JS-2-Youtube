dafd5a850d4c32b76771dd17776d20acd5c0e10d
shivani87838 <shivanis7513@gmail.com>
2025-07-29 10:07:56 +0000

    control flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,7 +1,15 @@
 // if
 const isUserLoggedIn=true;
-if(isUserLoggedIn){
-  
-}
 
-<,>
+const temp =41;
+if(temp===40){
+  console.log("Temp is less than 50");
+}
+else{
+console.log("Temp is more than 50");
+}
+console.log("executable");
+
+
+
+ //      <,>,<=,>=,==,!=,===,!==
