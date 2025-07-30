94bfcabc99d242a29e1cf71d6082d25d457e4fe7
shivani87838 <shivanis7513@gmail.com>
2025-07-30 15:46:08 +0000

    control flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -3,13 +3,52 @@ const isUserLoggedIn=true;
 
 const temp =41;
 if(temp===40){
-  console.log("Temp is less than 50");
+  //console.log("Temp is less than 50");
 }
 else{
-console.log("Temp is more than 50");
+//console.log("Temp is more than 50");
 }
-console.log("executable");
+//console.log("executable");
 
 
 
  //      <,>,<=,>=,==,!=,===,!==
+
+ /*
+ const score =200;
+ if(score>100){
+  const power ="fly";
+  console.log(`user power:${power}`);
+  
+ }
+  console.log(`user power:${power}`);
+  */
+
+ const balance = 1000;
+ //if(balance>500) console.log("test"),console.log("test2");
+ 
+ if(balance<500){
+ // console.log("less than 500"); 
+ }
+ else if (balance<750) {
+ // console.log("less than 750");
+ }
+ else if (balance<900) {
+ // console.log("less than 900");
+ }
+ else{
+ // console.log("less than 1200");
+ }
+
+
+ const userLoggedIn = true;
+ const debitCard = true;
+ const loggedInFromGoogle = false;
+ const loggedInFromEmail = true;
+ if (userLoggedIn && debitCard && 2==3) {
+  console.log("Allow user to buy courses");
+ }
+ if (loggedInFromEmail||loggedInFromGoogle) {
+  console.log("User logged in");
+ }
+ 
