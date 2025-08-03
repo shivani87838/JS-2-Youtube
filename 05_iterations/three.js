a25f3811b4737034ac4b8b62a0af1d655989c56d
shivani87838 <shivanis7513@gmail.com>
2025-08-03 15:37:54 +0000

    iterations

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -3,3 +3,22 @@
 // [{},{},{}]
 
 const arr =[1,2,3,4,5]
+for (const num of arr) {
+   // console.log(num);
+    
+}
+
+const greetings = "HelloWorld!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`);
+}
+
+
+//  Maps
+
+const map = new Map()
+map.set('IN',"India")
+map.set('USA',"United State of America")
+map.set('FR',"France")
+
+console.log(map);
